1b29163220195a40ac0e66d3ef95edaeaa270ef3
MickaelLepicier <mickael233@gmail.com>
2025-03-16 17:05:50 +0200

    Add OutletMailHeader.jsx

--- apps/mail/cmps/OutletMailHeader.jsx
+++ apps/mail/cmps/OutletMailHeader.jsx
@@ -1,10 +1,16 @@
 const { useState, useEffect, useRef } = React
 
-export function OutletMailHeader({ mails, setMails, onRemove, onRead, handleChange }) {
+export function OutletMailHeader({
+  mails,
+  setMails,
+  onRemove,
+  onRead,
+  handleChange
+}) {
   // TODOs:
   //V Create indbox btn for - Select all || UnSelect all
   //V If one of the mails is isSelected = true so I put - in the checkbox
-  
+
   // TODO keep working on that
   // Create Delete & Read || UnRead btns
 
@@ -13,20 +19,55 @@ export function OutletMailHeader({ mails, setMails, onRemove, onRead, handleChan
   // Read - check the first 50 mails if isRead Read || UnRead
   // (if 1 mail is !isRead so the option is isRead === true )
   const checkboxRef = useRef(null)
+  const btnsRef = useRef(null)
 
   const allSelected = mails.every((mail) => mail.isSelected)
 
   const someSelected = mails.some((mail) => mail.isSelected)
 
+  const isRead = true
+  const readIcon = isRead ? 'read' : 'unread'
+
   useEffect(() => {
     if (checkboxRef.current) {
       checkboxRef.current.indeterminate = !allSelected && someSelected
     }
+
+    if (btnsRef.current) {
+      btnsRef.current.style.visibility =
+        allSelected || someSelected ? 'visible' : 'hidden'
+      // btnsRef.current.style.visibility = 'visible'
+    }
   }, [allSelected, someSelected])
 
   function onSelectAll() {
-    setMails((prevMails) => prevMails.map((mail) => ({ ...mail, isSelected: !allSelected })))
+    setMails((prevMails) =>
+      prevMails.map((mail) => ({ ...mail, isSelected: !allSelected }))
+    )
   }
+
+  function onSetRead() {
+    // onRead(id)
+    const filteredMails = mails.filter((mail) => mail.isRead)
+    console.log('filteredMails: ', filteredMails)
+
+    mails.forEach((mail) => {
+      // TODO put more funcionality for unRead
+      if (mail.isSelected && !mail.isRead) {
+        onRead(mail.id)
+      }
+    })
+  }
+
+  function onSetRemove() {
+    // onRemove(id)
+    const filteredMails = mails.filter((mail) => mail.removedAt)
+  }
+
+  // function checkMails(type){
+  //  return mails.map(mail=> mail[type] === true)
+  // }
+
   // console.log('isChecked: ',isChecked)
 
   return (
@@ -40,6 +81,26 @@ export function OutletMailHeader({ mails, setMails, onRemove, onRead, handleChan
           onChange={onSelectAll}
         />
       </span>
+
+      <div className="outlet-btns-action" ref={btnsRef}>
+        <button className="btn-action" onClick={onSetRead}>
+          <img
+            src={`/apps/mail/img/icon/${readIcon}.png`}
+            alt="read-unread-icon"
+            className="read-unread-icon"
+          />
+        </button>
+
+        <div className="btn-separator">│</div>
+
+        <button className="btn-action" onClick={onSetRemove}>
+          <img
+            src="/apps/mail/img/icon/trash.png"
+            alt="trash-icon"
+            className="trash-icon"
+          />
+        </button>
+      </div>
 
       {/* <span className="mail-checkbox">
         <input
@@ -61,6 +122,8 @@ export function OutletMailHeader({ mails, setMails, onRemove, onRead, handleChan
         <button className="btn-action" onClick={() => onRemove(id)}>
           <img src="/apps/mail/img/icon/trash.png" alt="trash-icon" className="trash-icon" />
         </button>
+        </div>
+
       
       */}
     </section>
